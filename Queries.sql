edc3e4dced5c497560c78eab71531d0f73d1622f
Elias Gabriel <81771005+elias-gabriel@users.noreply.github.com>
2022-02-09 11:03:10 -0300

    SEMANA-02: Corrigindo Descrição da Query 06

--- Queries.sql
+++ Queries.sql
@@ -11,10 +11,9 @@ SELECT Nome_Livro from livro where Nome_Livro LIKE '%toolkit%'
 -- Consultar Nome do Livro, Autor e Editora onde possuem *todos os dados cadastrados
 Select Nome_Livro, Nome_Autor, Nome_Editora from livro_autor la inner join livro_editora le on la.ID_Livro = le.ID_Livro inner join livro l on le.ID_Livro = l.ID_Livro inner join autor a on la.ID_Autor = a.ID_Autor
                                                                                                              inner JOIN editora e on le.ID_Editora = e.ID_Editora
-
 -- Consultar Livros que Possuem entre 300 e 800 páginas
 select l.Nome_Livro, l.Idioma, le.N_Paginas from livro_editora le inner join livro l on le.ID_Livro = l.ID_Livro where N_Paginas BETWEEN 300 and 800 order by N_Paginas ASC
 
--- Consultar Livros que Possuem entre 300 e 800 páginas e são da editora Penguin Books
+-- Consultar Livros que Possuem entre 300 e 800 páginas que sejam da Editora Penguin Books ou o Idioma seja o Inglês
 select e.Nome_Editora, l.Nome_Livro, l.Idioma, le.N_Paginas from livro_editora le inner join livro l on le.ID_Livro = l.ID_Livro inner join editora e on le.ID_Editora = e.ID_Editora 
                                                            where N_Paginas BETWEEN 300 and 800 AND e.Nome_Editora = 'Penguin Books' OR l.Idioma like '%Inglês%' order by le.N_Paginas
